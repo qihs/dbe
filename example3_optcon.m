236fd08309a2351edf4add00136602241bb64e0d
qihs <qihongsh@gmail.com>
2022-05-12 22:35:29 +0000

    Update example3_optcon.m

--- example3_optcon.m
+++ example3_optcon.m
@@ -1,4 +1,4 @@
 function [c,ceq] = example3_optcon(x)
-% nonlinear constraint for the optmization prolem in example 3
+% Nonlinear constraint for the optmization problem in example 3
 c = [];
 ceq = x'*x - 1;
